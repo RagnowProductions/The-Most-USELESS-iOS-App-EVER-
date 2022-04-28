d48179f21caf1bf91c49b2e654f0a78ed436d2d6
Fenn <nicolaileff@icloud.com>
2022-04-27 19:51:07 -0500

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "power")
+                .imageScale(.large)
+                .foregroundColor(Color.brown)
+            Text("Power iOS Back On To Run App")
+        }
+    }
+}
